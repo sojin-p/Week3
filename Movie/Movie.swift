38b2f8a68d886f7416a4ba5e89dc3179c807443f
sojin-p <luv2hong@gmail.com>
2023-07-29 14:10:24 +0900

    Movie 구조체 파일로 따로 빼기

---
+++ Movie/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Week3
+//
+//  Created by 박소진 on 2023/07/29.
+//
+
+import Foundation
+
+struct Movie {
+    var mainTitle: String
+    var releaseDate: String
+    var runtime: Int
+    var overview: String
+    var rate: Float
+}
